366e9f818cc2c95d6378ead56f6c473f2e7d93c7
msk-asmb <msk@automatesmb.com>
2015-06-18 10:52:57 +0530

    updated database

--- admin.sql
+++ admin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2015 at 03:26 PM
+-- Generation Time: Jun 18, 2015 at 07:13 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -31,6 +31,10 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `username` varchar(30) DEFAULT NULL,
   `passcode` varchar(30) DEFAULT NULL,
   `mobileno` bigint(20) DEFAULT NULL,
+  `Address` varchar(20) NOT NULL,
+  `Street` varchar(20) NOT NULL,
+  `City` varchar(20) NOT NULL,
+  `Country` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
@@ -39,13 +43,13 @@ CREATE TABLE IF NOT EXISTS `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `passcode`, `mobileno`) VALUES
-(1, 'test', 'test', 9247566461),
-(2, 'raghav', 'madarasu', 9980424033),
-(3, 'siva', 'madarasu', 9036385347),
-(5, 'ayyar', 'madarasu', 9032566461),
-(6, 'Nilay', 'Nilay', 8147355615),
-(10, 'msrkrishna', 'madarasu', 7259680483);
+INSERT INTO `admin` (`id`, `username`, `passcode`, `mobileno`, `Address`, `Street`, `City`, `Country`) VALUES
+(1, 'test', 'test', 9247566461, '#66, Mahalaksmi nila', 'Tinfactory', 'Bangalore', 'India'),
+(2, 'raghav', 'madarasu', 9980424033, '9/237-1, madarasu ni', 'Nabikota', 'Kadapa', 'India'),
+(3, 'siva', 'madarasu', 9036385347, 'SR Residency', 'Pailayout', 'Bangalore', 'India'),
+(5, 'ayyar', 'madarasu', 9032566461, 'Abbanagari nilayam', 'Ramapuram', 'Chennai', 'India'),
+(6, 'Nilay', 'Nilay', 8147355615, 'HI-Tech Appartments', 'Sultanpalya', 'Bangalore', 'India'),
+(10, 'msrkrishna', 'madarasu', 7259680483, '#999 krishna home', 'K.R Puram', 'Bangalore', 'India');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
